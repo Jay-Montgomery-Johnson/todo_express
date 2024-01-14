f4c3bef479414f967f78e49f1016479cd8f35a80
Jay-Montgomery-Johnson <150203322+Jay-Montgomery-Johnson@users.noreply.github.com>
2024-01-14 23:01:27 +0000

    intermediate commit

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
 const express = require('express');
-const select = import('./db/connection.mjs')
+const cors = require('cors');
+const bp = require('body-parser');
+const pr = require('express-promise-router');
 const app = express();
+const db = require('./db/connection');
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    console.log('Here');
-    res.render("index", {text:"everybody"});
+app.use(cors());
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+
+
+app.get('/list', async (req, res, next) => {
+    /*
+    db.q().then((result) => {
+        console.log(result.rows);
+        console.log('res');
+        res.json({lists:result.rows});
+    });*/
+
+    //res.json({"lists":[{listId:"1", listName:"liserver2", items:[{itemId:"1", itemName:"a",completed:false},{itemId:"2", itemName:"b",completed:false},{itemId:"3", itemName:"c",completed:false}]},
+    //{listId:"2", listName:"listB", items:[{itemId:"1", itemName:"d",completed:false},{itemId:"2", itemName:"z",completed:false}]}]});
 });
 
-app.get('/list', (req, res) => {
-    console.log('todo list');
-    res.json({
-        "items": [
-            {"task":"Wash the dishes", "completed":"true"},
-            {"task":"Walk the dog", "completed":"false"}
-        ]
-    });
+app.post('/complete', (req, res) => {
+    console.log('complete: '+req.body);
+    res.send(req.body);
 });
 
-app.post('/add_item', (req, res) => {
+app.post('/delete', (req, res) => {
+    console.log('delete: '+req.body);
+    res.send(req.body);
+});
 
+
+
+app.post('/new_todo', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
 });
 
 //user routing
@@ -27,5 +45,5 @@ const userRouter = require('./routes/users.js');
 app.use('/users', userRouter);
 //end of the user route...
 
-app.listen(3000);
+app.listen(8080);
 
